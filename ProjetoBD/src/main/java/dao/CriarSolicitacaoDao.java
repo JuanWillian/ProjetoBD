ab2b80254f74c2a0ee4f5ec3213f395c9900ac8a
JuanWillian <juanwcaraujo28@gmail.com>
2025-06-05 01:31:02 -0300

    fix: mudando o tipo para java.sql.date

--- ProjetoBD/src/main/java/dao/CriarSolicitacaoDao.java
+++ ProjetoBD/src/main/java/dao/CriarSolicitacaoDao.java
@@ -11,30 +11,30 @@ import java.time.format.DateTimeFormatter;
 import connection.SingleConnection;
 
 public class CriarSolicitacaoDao {
-	
+
 	private Connection connection;
-	
+
 	public CriarSolicitacaoDao() {
 		connection = SingleConnection.getConnection();
 	}
-	
-	
-	public void cadastrarSolicitacao(String email, String senha, String dataReserva, String horarioInicio, String horarioFim, String nomeEspaco) throws ParseException, SQLException {
-		
+
+	public void cadastrarSolicitacao(String email, String senha, String dataReserva, String horarioInicio,
+			String horarioFim, String nomeEspaco) throws ParseException, SQLException {
+
 		Long idEspaco = getIdEspacoPorNome(nomeEspaco);
 		Long idUsuario = getIdUsuarioPorEmailSenha(email, senha);
 
-        DateTimeFormatter formatoEntrada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate data = LocalDate.parse(dataReserva, formatoEntrada);
-        DateTimeFormatter formatoSaida = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String dataSolicitacao = data.format(formatoSaida);
-        
-        LocalDate dataAtual = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String dataFormatada = dataAtual.format(formatter);
-		
+		DateTimeFormatter formatoEntrada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate data = LocalDate.parse(dataReserva, formatoEntrada);
+		DateTimeFormatter formatoSaida = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String dataSolicitacao = data.format(formatoSaida);
+
+		LocalDate dataAtual = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String dataFormatada = dataAtual.format(formatter);
+
 		String sql = "insert into solicitacao (idUsuarioFk, idEspacoFk, status, dataSolicitacao, dataReserva, horarioInicio, horarioFim) values (?, ?, DEFAULT, ?, ?, ?, ?)";
-		
+
 		try {
 			PreparedStatement insert = connection.prepareStatement(sql);
 			insert.setLong(1, idUsuario);
@@ -49,12 +49,12 @@ public class CriarSolicitacaoDao {
 			connection.rollback();
 			throw e;
 		}
-		
+
 	}
-	
+
 	private Long getIdEspacoPorNome(String nomeEspaco) throws SQLException {
 		String sql = "select idEspaco from espaco where nome = ?";
-		
+
 		PreparedStatement statement = connection.prepareStatement(sql);
 		statement.setString(1, nomeEspaco);
 		ResultSet resultado = statement.executeQuery();
@@ -64,31 +64,31 @@ public class CriarSolicitacaoDao {
 			throw new SQLException("Espaço não encontrado: " + nomeEspaco);
 		}
 	}
-	
+
 	public Long getIdUsuarioPorEmailSenha(String email, String senha) throws SQLException {
-		 String sql = "select u.idUsuario from usuario u join email e on u.idUsuario = e.idUsuarioFk where e.enderecoEmail = ? and e.senha = ?";
-		 
-			 PreparedStatement statement = connection.prepareStatement(sql);
-			 statement.setString(1, email);
-			 statement.setString(2, senha); 
-			 ResultSet resultado = statement.executeQuery();
-			 if (resultado.next()) {
-			        return resultado.getLong("idUsuario");
-			    } else {
-			        throw new SQLException("Usuário não encontrado para o email e senha informados.");
-			    }
+		String sql = "select u.idUsuario from usuario u join email e on u.idUsuario = e.idUsuarioFk where e.enderecoEmail = ? and e.senha = ?";
+
+		PreparedStatement statement = connection.prepareStatement(sql);
+		statement.setString(1, email);
+		statement.setString(2, senha);
+		ResultSet resultado = statement.executeQuery();
+		if (resultado.next()) {
+			return resultado.getLong("idUsuario");
+		} else {
+			throw new SQLException("Usuário não encontrado para o email e senha informados.");
+		}
 	}
-	
+
 	public void cadastraAuditoria(String email, String senha) throws SQLException, ParseException {
-		
+
 		Long idUsuario = getIdUsuarioPorEmailSenha(email, senha);
-		
+
 		LocalDate dataAtual = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String dataFormatada = dataAtual.format(formatter);
-		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String dataFormatada = dataAtual.format(formatter);
+
 		String sql = "insert into auditoria (idUsuarioFk,dataAcao,acao) values (?,?,?)";
-		
+
 		try {
 			PreparedStatement insert = connection.prepareStatement(sql);
 			insert.setLong(1, idUsuario);
@@ -101,31 +101,31 @@ public class CriarSolicitacaoDao {
 			throw e;
 		}
 	}
-	
-	public void cadastrarSolicitacaoPorId(String email, String senha, String dataReserva, String horarioInicio, String horarioFim, Long idEspaco) throws ParseException, SQLException {
-        Long idUsuario = getIdUsuarioPorEmailSenha(email, senha);
-        DateTimeFormatter formatoEntrada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate data = LocalDate.parse(dataReserva, formatoEntrada);
-        DateTimeFormatter formatoSaida = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String dataSolicitacao = data.format(formatoSaida);
-        LocalDate dataAtual = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String dataFormatada = dataAtual.format(formatter);
-        String sql = "insert into solicitacao (idUsuarioFk, idEspacoFk, status, dataSolicitacao, dataReserva, horarioInicio, horarioFim) values (?, ?, DEFAULT, ?, ?, ?, ?)";
-        try {
-            PreparedStatement insert = connection.prepareStatement(sql);
-            insert.setLong(1, idUsuario);
-            insert.setLong(2, idEspaco);
-            insert.setString(3, dataFormatada);
-            insert.setString(4, dataSolicitacao);
-            insert.setString(5, horarioInicio);
-            insert.setString(6, horarioFim);
-            insert.executeUpdate();
-            connection.commit();
-        } catch (Exception e) {
-            connection.rollback();
-            throw e;
-        }
-    }
+
+	public void cadastrarSolicitacaoPorId(String email, String senha, String dataReserva, String horarioInicio,
+			String horarioFim, Long idEspaco) throws ParseException, SQLException {
+		Long idUsuario = getIdUsuarioPorEmailSenha(email, senha);
+		java.sql.Date dataReservaSql = null;
+		try {
+			java.util.Date utilDate = new java.text.SimpleDateFormat("dd/MM/yyyy").parse(dataReserva);
+			dataReservaSql = new java.sql.Date(utilDate.getTime());
+		} catch (ParseException e) {
+			throw new SQLException("Data em formato inválido. Use dd/MM/yyyy");
+		}
+		String sql = "insert into solicitacao (idUsuarioFk, idEspacoFk, status, dataSolicitacao, dataReserva, horarioInicio, horarioFim) values (?, ?, DEFAULT, CURRENT_DATE, ?, ?, ?)";
+		try {
+			PreparedStatement insert = connection.prepareStatement(sql);
+			insert.setLong(1, idUsuario);
+			insert.setLong(2, idEspaco);
+			insert.setDate(3, dataReservaSql); 
+			insert.setString(4, horarioInicio);
+			insert.setString(5, horarioFim);
+			insert.executeUpdate();
+			connection.commit();
+		} catch (Exception e) {
+			connection.rollback();
+			throw e;
+		}
+	}
 
 }
